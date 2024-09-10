cd9fc182199165db2830c4cd1f52d94222f573a7
Revy <162366071+teoto4@users.noreply.github.com>
2024-09-11 00:21:00 +0300

    chore(main.js) back comment task

--- js/main.js
+++ js/main.js
@@ -6,35 +6,35 @@
 Далі програма повторно запитує слово для перекладу і так по колу.
 Для завершення програми юзер має ввести команду: #exit
  */
-// const map = new Map([
-//   ["first", "превый"],
-//   ["second", "второй"],
-//   ["third", "третий"],
-// ]);
+const map = new Map([
+  ["first", "превый"],
+  ["second", "второй"],
+  ["third", "третий"],
+]);
 
-// function translate(getWord) {
-//   if (map.has(getWord) === true) {
-//     alert(map.get(getWord));
-//   } else {
-//     alert("Добавьте перевод");
-//     let add = prompt("Добавить");
-//     map.set(getWord, add);
-//     alert(`Слово ${getWord} добавлено. Перевод: ${add}`);
-//   }
-// }
+function translate(getWord) {
+  if (map.has(getWord) === true) {
+    alert(map.get(getWord));
+  } else {
+    alert("Добавьте перевод");
+    let add = prompt("Добавить");
+    map.set(getWord, add);
+    alert(`Слово ${getWord} добавлено. Перевод: ${add}`);
+  }
+}
 
-// let exit = false;
-// while (exit === false) {
-//   const getWord = prompt("Скажи слово или выйди = #exit");
-//   console.log(getWord);
+let exit = false;
+while (exit === false) {
+  const getWord = prompt("Скажи слово или выйди = #exit");
+  console.log(getWord);
 
-//   if (getWord === "#exit") {
-//     alert("Выход из программы");
-//     exit = true;
-//   } else {
-//     translate(getWord);
-//   }
-// }
+  if (getWord === "#exit") {
+    alert("Выход из программы");
+    exit = true;
+  } else {
+    translate(getWord);
+  }
+}
 /*2.
 (Set) У вас є два масиви з числами. Необхідно знайти всі числа, які є одночасно в обох масивах.
 const array1 = [1, 2, 3, 4, 5, 6];
